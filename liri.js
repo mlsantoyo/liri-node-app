02d9b04c91d9fb3f344c90ed80deb4a2d4fb4cc2
Marcy <mlsantoyo@att.net>
2017-07-13 14:29:57 -0400

    imdb works

--- liri.js
+++ liri.js
@@ -1,7 +1,8 @@
 // require nmps
 var Twitter = require('twitter');
-var fs = require('fs');
+// var fs = require('fs');
 var Spotify = require('node-spotify-api');
+var request = require("request");
 
 
 
@@ -14,7 +15,7 @@ var client = new Twitter({
 	consumer_secret: twitterKeys.consumer_secret,
 	access_token_key: twitterKeys.access_token_key,
 	access_token_secret: twitterKeys.access_token_secret
-	});
+});
 //	test to see if the keys are working
 	// console.log(client);
 
@@ -31,7 +32,7 @@ for (var i=3; i < nodeArgs.length; i++){
   } else{
     input = input + nodeArgs[i];
   }
-}
+};
 
 
 //use switch case to list all the commands: my-tweets, spotify-this-song, movie-this, do-what-it-says
@@ -41,38 +42,42 @@ switch (command) {
 	break;
 
 	case 'spotify-this-song':
-	 if (input){
+  if (input){
     spotify(input);
   } else {
     spotify('The Sign Ace of Base');
   }
-	break;
+  break;
 
-	case 'movie-this':
-	movie();
-	break;
+  case 'movie-this':
+  if (input){
+    movie(input);
+  } else {
+    movie('Mr Nobody');
+  }
+  break;
 
-	case 'do-what-it-says':
-	dwis();
-	break;
+  case 'do-what-it-says':
+  dwis();
+  break;
 };
 
 //node liri.js my-tweets shows last 20 tweets and when they were created in your terminal/bash window
 function myTweets() {
 	// var client = new Twitter;
-    var params = {screen_name: 'MaltNectar', count: 20};
-    client.get('statuses/user_timeline', params, function(error, tweetData, response) {
-      if (!error) {
-        
-        console.log('<<<<<<<<<<<<<<<< My Tweets >>>>>>>>>>>>>>>>');
-        tweetData.forEach(function(obj) {
-          console.log('--------------------------');
-          console.log('Time: ' + obj.created_at);
-          console.log('Tweet: ' + obj.text);
-          console.log('--------------------------');
-          
-        });
-        console.log('===========================================');
+  var params = {screen_name: 'MaltNectar', count: 20};
+  client.get('statuses/user_timeline', params, function(error, tweetData, response) {
+    if (!error) {
+
+      console.log('<<<<<<<<<<<<<<<< My Tweets >>>>>>>>>>>>>>>>');
+      tweetData.forEach(function(obj) {
+        console.log('--------------------------');
+        console.log('Time: ' + obj.created_at);
+        console.log('Tweet: ' + obj.text);
+        console.log('--------------------------');
+
+      });
+      console.log('===========================================');
         // console.log(' ');
         // console.log(tweets);
 
@@ -81,28 +86,54 @@ function myTweets() {
         console.log(error);
       }
     });
-  };
+};
 
-  function spotify(input) {
+function spotify(input) {
 
-  	var spotify = new Spotify({
-  		id: '5fbd873d512145ef99f5bd94287ee12e',
-  		secret: '95b557199e174647afcd06b5c233fc82'
-  	});
+ var spotify = new Spotify({
+  id: '5fbd873d512145ef99f5bd94287ee12e',
+  secret: '95b557199e174647afcd06b5c233fc82'
+});
 
-  	spotify.search({ type: 'track', query: input }, function(err, data) {
-  		if (err) {
-  			return console.log('Error occurred: ' + err);
-  		}
+ spotify.search({ type: 'track', query: input }, function(err, data) {
+  if (err) {
+   return console.log('Error occurred: ' + err);
+ }
 
-  		console.log('================ Song Info ================');
-        console.log('Artist: ' + data.tracks.items[0].album.artists[0].name);
-        console.log('Name: ' + data.tracks.items[0].name);
-        console.log('Link: ' + data.tracks.items[0].preview_url);
-        console.log('Album: ' + data.tracks.items[0].album.name);
-        console.log('===========================================');
+ console.log('================ Song Info ================');
+ console.log('Artist: ' + data.tracks.items[0].album.artists[0].name);
+ console.log('Name: ' + data.tracks.items[0].name);
+ console.log('Link: ' + data.tracks.items[0].preview_url);
+ console.log('Album: ' + data.tracks.items[0].album.name);
+ console.log('===========================================');
 
     // console.log(JSON.stringify(data, null, " 	"));
-  	});
+  });
 
+};
+
+function movie(input) {
+
+ request("http://www.omdbapi.com/?t=" + input + "&y=&plot=short&tomatoes=true&apikey=40e9cece", function(error, response, body) {
+
+
+    if(!error && response.statusCode == 200){
+
+      var body = JSON.parse(body);
+
+      console.log("Title: " + body.Title);
+      console.log("Release Year: " + body.Year);
+      console.log("IMdB Rating: " + body.imdbRating);
+      console.log("Country: " + body.Country);
+      console.log("Language: " + body.Language);
+      console.log("Plot: " + body.Plot);
+      console.log("Actors: " + body.Actors);
+      console.log("Rotten Tomatoes Rating: " + body.tomatoRating);
+      console.log("Rotten Tomatoes URL: " + body.tomatoURL);
+
+    } else {
+      console.log('Error occurred.')
+    };
+   }); 
   };
+  
